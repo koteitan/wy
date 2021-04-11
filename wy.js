c8d7de5648a39d9d7942a09ca2607c000097cc0a
koteitan <ukaibutton@mac.com>
2021-04-11 17:18:31 +0900

    add some functions

--- wy.js
+++ wy.js
@@ -1,13 +1,13 @@
-var Wy=function(a){
+var Wyseq=function(a){
   if(Array.isArray(a)){
-    this.s=a.clone();
+    this.seq=a.clone();
   }else if(typeof a == "string"){
-    b = Wy.parse(a);
-    this.s = b.s.clone();
+    b = Wyseq.parse(a);
+    this.seq = b.seq.clone();
   }
 }
 
-Wy.parse=function(instr){
+Wyseq.parse=function(instr){
   instr=instr.replace(/^[\n\s]/g, "");
   instr=instr.replace(/[\n\s]$/g, "");
   instr=instr.replace(/[\n\s][\n\s]*/g, " ");
@@ -16,15 +16,75 @@ Wy.parse=function(instr){
   for(var i=0;i<a.length;i++){
     a[i]=parseInt(a[i]);
   }
-  return new Wy(a);
+  return new Wyseq(a);
 }
 
-Wy.prototype.toString=function(){
+Wyseq.prototype.toString=function(){
   var str="";
-  if(this.s.length!=0) str+=this.s[0];
-  for(var i=1;i<this.s.length;i++){
-    str+=","+this.s[i];
+  if(this.seq.length!=0) str+=this.seq[0];
+  for(var i=1;i<this.seq.length;i++){
+    str+=","+this.seq[i];
   }
   return str;
 }
+
+Wyseq.ananode=function(){
+  var a=new Array(this.seq.length);
+  for(var i=0;i<this.seq.length;i++){
+    a[i]=new Wynode(this.seq[i]);
+  }
+  this.s=[a];
+
+  s=this.s;
+  while(true){
+    var ys=s.length;
+    var xs=s[0].length;
+    var finished=true;
+    for(var x=0;x<xs;x++){
+      for(var y=ys;y>=0;y--){
+        if(s[y][x]==1)break;
+        if(s[y][x]!=0){
+          finished=false;
+          break;
+        }
+      }//for y
+      if(!finished) break;
+    }//for x
+    if(finished) return;
+    // not finished yet
+    
+    //find 
+    for(var y=0;y<ys;y++){
+      
+    }
+  }//while
+}
+
+var Wynode=function(seq){
+  if(typeof seq=="undefined" || seq==null) this.seq=new Wy();
+  this.seq=seq; //sequence this belongs
+  this.seq.push(this); // register the node to the sequence
+  if(typeof v!="undefined") this.v=v; // value
+  if(typeof v!="undefined") this.v=v;; // i[d]=dth dimension of index
+  this.p; // parent node
+  this.d; // diff node
+  this.rightleg; // right leg
+  this.leftleg; // left leg
+}
+
+var Wyseq.prototype.at=function(i){
+  for(var t=0;t<this.nodelist.length;t++){
+    if(this.nodelist[t].i==i) return this.nodelist[t];
+  }
+  return null;
+}
+
+var Wynode.prototype.getleft=function(n){
+  if(this.rightleg==null)
+}
+
+var Wynode.prototype.getparent=function(c){
+  
+}
+
 
